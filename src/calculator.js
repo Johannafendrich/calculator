36e2351630eb870f18b761e5b3aa482acf66ea5a
Johannafendrich <fendrichjohanna@gmail.com>
2020-02-26 11:16:15 +0100

    Add calculator functions and style

--- src/calculator.js
+++ src/calculator.js
@@ -1,9 +1,31 @@
-const calculatorOutput = document.querySelector(".calculator__output");
-calculatorOutput.value = "42";
+"use strict";
 
+const calculatorOutput = document.querySelector(".calculator__output");
+calculatorOutput.value = multiply(10, 5);
+
+function addition(numberOne, numberTwo) {
+  return numberOne + numberTwo;
+}
+
+function subtract(numberOne, numberTwo) {
+  return numberOne - numberTwo;
+}
+
+function divide(numberOne, numberTwo) {
+  return numberOne / numberTwo;
+}
+
+function multiply(numberOne, numberTwo) {
+  return numberOne * numberTwo;
+}
+
+// addition =
+
+/*
 const calculatorInputs = document.querySelectorAll(".calculator__input");
 calculatorInputs.forEach(function(calculatorInput) {
   calculatorInput.addEventListener("click", function() {
     calculatorOutput.value = calculatorInput.innerHTML;
   });
 });
+*/
